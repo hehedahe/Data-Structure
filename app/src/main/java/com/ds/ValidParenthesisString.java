250151e67a1823cf47d838957b72e03f21aae621
hehedahe <smyekgo@gmail.com>
2022-03-06 04:41:13 +0900

    VPS

--- app/src/main/java/com/ds/ValidParenthesisString.java
+++ app/src/main/java/com/ds/ValidParenthesisString.java
@@ -2,28 +2,45 @@ package com.ds;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class ValidParenthesisString {
 
-  public static String[] stack;
-  public static int size = 0;
+  static Stack<Character> stack = new Stack<Character>();
 
   public static void main(String[] args) throws Exception {
-    // 콘솔 창에서 입력한 데이터를 읽는다.
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    // 문자열로 이루어진 배열들 => 문제들을 담을 바구니
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder sb = new StringBuilder();
 
-    // StringBuilder 배열에 있는 데이터들을 하나하나 읽어줄 토큰
-    StringTokenizer st;
-
-    // 명령의 수 
     int N = Integer.parseInt(br.readLine());
-    stack = new String[N];
 
+    while (N-- > 0) {
+
+      sb.append(answer(br.readLine())).append("\n");
+
+    }
+    System.out.println(sb);
   }
 
+  public static String answer(String str) {
+    char[] chr = str.toCharArray();
 
+    for (int i = 0; i < chr.length; i++) {
+      if (chr[i] == '(') {
+        stack.push(chr[i]);
+      } else if (stack.empty()) {
+        return "NO";
+      } else {
+        stack.pop();
+      }
+    }
+
+    if (stack.empty()) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+
+  }
 }
