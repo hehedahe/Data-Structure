68b1b255dd2e9e8cc923ad17d30ece14feb3b31b
hehedahe <smyekgo@gmail.com>
2022-03-11 01:42:04 +0900

    Queue

--- app/src/main/java/com/ds/Queue.java
+++
@@ -1,8 +0,0 @@
-package com.ds;
-
-
-// Queue구조 [ FIFO ] first in first out 
-public class Queue {
-
-
-}
---
+++ app/src/main/java/com/ds/Queue10845.java
@@ -0,0 +1,15 @@
+package com.ds;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Queue구조 [ FIFO ] first in first out 
+public class Queue10845 {
+
+
+  Queue<Integer> queue = new LinkedList<>();
+
+  public static void main(String[] args) {
+
+  }
+}
