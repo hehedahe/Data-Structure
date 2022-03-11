cc855fb707cbf2c04cd8d0ad5fdbb7d9c0f77d85
hehedahe <smyekgo@gmail.com>
2022-03-12 02:21:53 +0900

    question

--- app/src/main/java/com/ds/Queue10845.java
+++ app/src/main/java/com/ds/Queue10845.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 // Queue구조 [ FIFO ] first in first out 
 public class Queue10845 {
 
-
+  // LinkedList랑 붙이는 이유는???
   Queue<Integer> queue = new LinkedList<>();
 
   public static void main(String[] args) {
