7487774a3b7adc1105393f9d3fdaf2af0816c85e
hehedahe <smyekgo@gmail.com>
2022-03-16 23:55:11 +0900

    Integer / String 차이 Test

---
+++ app/src/main/java/com/ds/Test.java
@@ -0,0 +1,52 @@
+package com.ds;
+
+public class Test {
+
+
+  // null --> null Type
+  public static void main(String[] args) {
+    User user = new User();
+    user.setId(null);
+    user.setName("채다해");
+    // ---------------------------------- db 
+
+    //        if ("1".equals(user.getId())) {
+    //    if (user.getId().equals("1")) {
+
+    //    if (1 == user.getId()) {
+    if (user.getId() == 1) {
+
+      System.out.println(user.getId());
+      System.out.println(user.getName());
+    }
+
+
+  }
+
+
+  static class User {
+    Integer id;
+    //    String id;
+    String name;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public Integer getId() {
+      //    public String getId() {
+      return id;
+    }
+
+    public void setId(Integer id) {
+      //    public void setId(String id) {
+      this.id = id;
+    }
+
+  }
+
+}
