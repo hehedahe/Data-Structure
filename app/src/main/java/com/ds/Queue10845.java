534d90fc090daadef43c8159bd0546e05e7c907b
hehedahe <smyekgo@gmail.com>
2022-07-17 17:12:55 +0900

    Queue : First In First Out

--- app/src/main/java/com/ds/Queue10845.java
+++ app/src/main/java/com/ds/Queue10845.java
@@ -1,16 +1,42 @@
 package com.ds;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 // Queue구조 [ FIFO ] first in first out 
 public class Queue10845 {
 
-  // LinkedList랑 붙이는 이유는???
-  Queue<Integer> queue = new LinkedList<>();
+  static Queue<Integer> queue = new LinkedList<>();
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 
     // Queue 도 Stack 과 비슷한 방식으로 구현할 것
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st;
+
+    Integer N = Integer.parseInt(br.readLine());
+
+
   }
-}
+
+  public static void push(int num) {
+    queue.add(num);
+  }
+
+  public static int pop() {
+    if(queue.isEmpty()) {
+      return -1;
+    }
+    return queue.poll();
+  }
+
+  // public static int size() 
+
+} 
